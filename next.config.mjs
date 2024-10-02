f10c96c6d47aa319310a990ce5da7e1adfb00798
Bahaidahar <krutoyBaha@bk.ru>
2024-10-02 23:08:37 +0500

    some fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,8 @@
 import createNextIntlPlugin from 'next-intl/plugin';
  
-let withNextIntl;
-try {
-  withNextIntl = createNextIntlPlugin({
-    defaultLocale: 'ru',
-    locales: ['en', 'ru', 'kz'],
-  });
-} catch (err) {
-  console.error("Error initializing next-intl plugin:", err);
-  throw new Error("Failed to initialize next-intl plugin");
-}
+const withNextIntl = createNextIntlPlugin();
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true, 
-};
+const nextConfig = {};
  
 export default withNextIntl(nextConfig);

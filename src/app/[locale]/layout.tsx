2cf08bddc894665c0d800fc6fa4fb4cbda7e02f5
Bahaidahar <krutoyBaha@bk.ru>
2024-10-03 00:03:59 +0500

    fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import "../globals.css";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import AnimatedBackground from "@/components/AnimatedBackground";
